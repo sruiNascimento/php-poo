9cb33b3c1a48cbff705ede7230f6246d64566825
Rui Nascimento <sruy19@gmail.com>
2021-09-07 08:38:56 -0300

    Testando as funçoes da classe Conta

--- MainConta.php
+++ MainConta.php
@@ -15,11 +15,27 @@ $acc2->saldo = 1500;
 
 echo var_dump($acc1) . PHP_EOL;
 echo var_dump($acc2) . PHP_EOL;
-//Iterando sobre os atributos 
-foreach ($acc1 as $data){
-    echo $data . PHP_EOL;
-}
-foreach ($acc2 as $data){
-    echo $data . PHP_EOL;
-}
+// //Iterando sobre os atributos 
+// foreach ($acc1 as $data){
+//     echo $data . PHP_EOL;
+// }
+// foreach ($acc2 as $data){
+//     echo $data . PHP_EOL;
+// }
 
+//Sacando 10 reais da $acc1
+$acc1->sacar(10);
+//Apresentando todos os dados da $acc1
+echo $acc1->getToString();
+
+$acc2->depositar(0);
+
+$acc2->sacar(1500);
+echo $acc2->getToString();
+
+$acc2->depositar(100);
+//echo $acc2->getToString();
+$acc2->transfere($acc1, 50);
+
+echo $acc2->getToString();
+echo $acc1->getToString();
